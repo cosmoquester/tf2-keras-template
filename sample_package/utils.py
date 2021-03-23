fix: Update TPU mixed precision type

sample_package/utils.py
@@ -2,7 +2,7 @@ import logging
 import os
 import random
 import sys
-from typing import Callable, Iterable, Optional
+from typing import Callable, Iterable, Optional, Union
 
 import numpy as np
 import tensorflow as tf
@@ -25,9 +25,9 @@ class LRScheduler(tf.keras.optimizers.schedules.LearningRateSchedule):
         self.decreasing_delta = (max_learning_rate - min_learning_rate) / (total_steps - self.warmup_steps)
         self.max_learning_rate = tf.cast(max_learning_rate, tf.float32)
         self.min_learning_rate = tf.cast(min_learning_rate, tf.float32)
-        self.total_steps = total_steps
 
-    def __call__(self, step: tf.Tensor) -> tf.Tensor:
+    def __call__(self, step: Union[int, tf.Tensor]) -> tf.Tensor:
+        step = tf.cast(step, tf.float32)
         lr = tf.reduce_min([step * self.increasing_delta, self.max_learning_rate - step * self.decreasing_delta])
         return tf.reduce_max([lr, self.min_learning_rate])
 

scripts/train.py
@@ -37,11 +37,6 @@ if __name__ == "__main__":
 
     logger = get_logger()
 
-    if args.mixed_precision:
-        policy = tf.keras.mixed_precision.experimental.Policy("mixed_float16")
-        tf.keras.mixed_precision.experimental.set_policy(policy)
-        logger.info("Use Mixed Precision FP16")
-
     if args.seed:
         set_random_seed(args.seed)
         logger.info(f"Set random seed to {args.seed}")
@@ -53,13 +48,19 @@ if __name__ == "__main__":
             fout.write(f"{k}: {v}\n")
     tf.io.gfile.copy(args.model_config_path, path_join(args.output_path, "model_config.json"))
 
-    # Construct Dataset
-    dataset_files = tf.io.gfile.glob(args.dataset_path)
-    if not dataset_files:
-        logger.error("[Error] Dataset path is invalid!")
-        sys.exit(1)
-
     with get_device_strategy(args.device).scope():
+        if args.mixed_precision:
+            mixed_type = "mixed_bfloat16" if args.device == "TPU" else "mixed_float16"
+            policy = tf.keras.mixed_precision.experimental.Policy(mixed_type)
+            tf.keras.mixed_precision.experimental.set_policy(policy)
+            logger.info("Use Mixed Precision FP16")
+
+        # Construct Dataset
+        dataset_files = tf.io.gfile.glob(args.dataset_path)
+        if not dataset_files:
+            logger.error("[Error] Dataset path is invalid!")
+            sys.exit(1)
+
         dataset = get_dataset(dataset_files).shuffle(args.shuffle_buffer_size)
         train_dataset = dataset.skip(args.num_dev_dataset).batch(args.batch_size)
         dev_dataset = dataset.take(args.num_dev_dataset).batch(max(args.batch_size, args.dev_batch_size))