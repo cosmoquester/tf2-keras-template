c899b41937b168c266b32f09d33a3da68f992a6a
박상준 <psj8252@naver.com>
2021-05-01 15:56:55 +0900

    feat: Add logging callback

--- sample_package/utils.py
+++ sample_package/utils.py
@@ -2,15 +2,14 @@ import logging
 import os
 import random
 import sys
-from typing import Callable, Iterable, Optional, Union
+from typing import Dict, Iterable, Optional, Union
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import backend as K
 
 
 class LRScheduler(tf.keras.optimizers.schedules.LearningRateSchedule):
-    """ Schedule learning rate linearly from max_learning_rate to min_learning_rate. """
+    """Schedule learning rate linearly from max_learning_rate to min_learning_rate."""
 
     def __init__(
         self,
@@ -32,8 +31,50 @@ class LRScheduler(tf.keras.optimizers.schedules.LearningRateSchedule):
         return tf.reduce_max([lr, self.min_learning_rate])
 
 
+class LoggingCallback(tf.keras.callbacks.Callback):
+    """Callback to log losses and metrics during training"""
+
+    def __init__(self, logger: logging.Logger, logging_step: int = 100):
+        self.logger = logger
+        self.logging_step = logging_step
+
+        self.step = None
+        self.epoch = 1
+        self.total_step = None
+        self.logs = {}
+
+    def on_batch_end(self, batch: int, logs: Dict[str, float]):
+        """
+        On batch end, add values of loss and metric. When step is logging step, print averaged values and reset.
+
+        :param batch: current batch index
+        :param logs: dictionary containing losses and metrics
+        """
+        self.step = batch + 1
+        for name, value in logs.items():
+            self.logs[name] = self.logs.get(name, 0) + value
+        if self.step % self.logging_step == 0:
+            values = ", ".join(f"{name}: {value / self.logging_step:.4f}" for name, value in self.logs.items())
+            self.logger.info(f"{self.epoch} epoch, {self.step} / {self.total_step} step | " + values)
+            self.logs = {}
+
+    def on_epoch_end(self, epoch: int, logs: Dict[str, float]):
+        """
+        Print epoch losses and metrics on epoch end.
+
+        :param batch: current epoch index
+        :param logs: dictionary containing losses and metrics
+        """
+        values = ", ".join(f"{name}: {value:.4f}" for name, value in logs.items())
+        self.logger.info(f"{self.epoch} epoch | " + values)
+
+        self.total_step = self.step
+        self.epoch += 1
+        self.logs = {}
+
+
 def get_logger() -> logging.Logger:
-    """ Return logger for logging """
+    """Return logger for logging"""
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler(sys.stdout)
@@ -43,21 +84,21 @@ def get_logger() -> logging.Logger:
 
 
 def path_join(*paths: Iterable[str]) -> str:
-    """ Join paths to string local paths and google storage paths also """
+    """Join paths to string local paths and google storage paths also"""
     if paths[0].startswith("gs://"):
         return "/".join((path.strip("/") for path in paths))
     return os.path.join(*paths)
 
 
 def set_random_seed(seed: int):
-    """ Set random seed for random / numpy / tensorflow """
+    """Set random seed for random / numpy / tensorflow"""
     random.seed(seed)
     np.random.seed(seed)
     tf.random.set_seed(seed)
 
 
 def get_device_strategy(device) -> tf.distribute.Strategy:
-    """ Return tensorflow device strategy """
+    """Return tensorflow device strategy"""
     # Use TPU
     if device.upper() == "TPU":
         resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu=os.environ["TPU_NAME"])
