fix: Update mixed precision functions

scripts/train.py
@@ -52,8 +52,8 @@ if __name__ == "__main__":
     with get_device_strategy(args.device).scope():
         if args.mixed_precision:
             mixed_type = "mixed_bfloat16" if args.device == "TPU" else "mixed_float16"
-            policy = tf.keras.mixed_precision.experimental.Policy(mixed_type)
-            tf.keras.mixed_precision.experimental.set_policy(policy)
+            policy = tf.keras.mixed_precision.Policy(mixed_type)
+            tf.keras.mixed_precision.set_global_policy(policy)
             logger.info("Use Mixed Precision FP16")
 
         # Construct Dataset